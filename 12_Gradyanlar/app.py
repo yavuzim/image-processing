6bcebc30e9744eb2653c9b030844635ac1fd8b8e
yavuzim <kyavuzimer@gmail.com>
2023-04-10 17:33:06 +0300

    gradyanlar

---
+++ 12_Gradyanlar/app.py
@@ -0,0 +1,18 @@
+import cv2 as cv
+import matplotlib.pyplot as plt
+
+img = cv.imread("sudoku.jpg", 0)
+
+plt.figure(), plt.imshow(img, cmap = "gray"), plt.axis("off"), plt.title("Orijinal")
+
+# x gradyan.
+sobelX = cv.Sobel(img, ddepth = cv.CV_16S, dx = 1, dy = 0, ksize = 5)
+plt.figure(), plt.imshow(sobelX, cmap = "gray"), plt.axis("off"), plt.title("X Gradyan")
+
+# y gradyan.
+sobelY = cv.Sobel(img, ddepth = cv.CV_16S, dx = 0, dy = 1, ksize = 5)
+plt.figure(), plt.imshow(sobelY, cmap = "gray"), plt.axis("off"), plt.title("Y Gradyan")
+
+# laplacian gradyan.(x-y)
+lap = cv.Laplacian(img, ddepth = cv.CV_16S, ksize = 5)
+plt.figure(), plt.imshow(lap, cmap = "gray"), plt.axis("off"), plt.title("Laplacian")
